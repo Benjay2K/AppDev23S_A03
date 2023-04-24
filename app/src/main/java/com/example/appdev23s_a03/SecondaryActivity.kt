5fb301da7aaa93250826e13434d4d5624962d3c6
Benjay2K <benilepuschitz@gmail.com>
2023-04-24 21:10:06 +0200

    Removed if condition for google maps

--- app/src/main/java/com/example/appdev23s_a03/SecondaryActivity.kt
+++ app/src/main/java/com/example/appdev23s_a03/SecondaryActivity.kt
@@ -32,11 +32,7 @@ class SecondaryActivity : AppCompatActivity() {
             val uri = "geo:0,0?q=$address"
             val intent = Intent(Intent.ACTION_VIEW, Uri.parse(uri))
             intent.setPackage("com.google.android.apps.maps")
-            if (intent.resolveActivity(packageManager) != null) {
-                startActivity(intent)
-            } else {
-                Toast.makeText(this, "Google Maps is not installed", Toast.LENGTH_SHORT).show()
-            }
+            startActivity(intent)
         }
 
         returnButton.setOnClickListener {
