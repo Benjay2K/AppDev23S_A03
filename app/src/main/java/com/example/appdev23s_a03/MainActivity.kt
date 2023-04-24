958bf6c50e4ec417df9c24db3a6d1c5340eb8e3b
Benjay2K <benilepuschitz@gmail.com>
2023-04-25 01:16:21 +0200

    Music player

--- app/src/main/java/com/example/appdev23s_a03/MainActivity.kt
+++ app/src/main/java/com/example/appdev23s_a03/MainActivity.kt
@@ -8,11 +8,16 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.app.Service
+import android.media.MediaPlayer
+import android.os.IBinder
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var editAddressInput: EditText
     private lateinit var activity2Button: Button
+    private lateinit var startMusic: Button
+    private lateinit var stopMusic: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +25,8 @@ class MainActivity : AppCompatActivity() {
 
         editAddressInput = findViewById(R.id.editAddressInput)
         activity2Button = findViewById(R.id.activity2Button)
+        startMusic = findViewById(R.id.startMusic)
+        stopMusic = findViewById(R.id.stopMusic)
 
         activity2Button.setOnClickListener {
             val address = editAddressInput.text.toString()
@@ -27,6 +34,19 @@ class MainActivity : AppCompatActivity() {
             intent.putExtra("ADDRESS", address)
             startActivity(intent)
         }
+
+        startMusic.setOnClickListener{
+            Intent(applicationContext, MusicService::class.java).also {
+                startService(it)
+            }
+        }
+
+        stopMusic.setOnClickListener{
+            Intent(applicationContext, MusicService::class.java).also {
+                stopService(it)
+            }
+        }
+
     }
 
     override fun onStart() {
@@ -57,4 +77,25 @@ class MainActivity : AppCompatActivity() {
         super.onRestart()
         Log.d("A4", "onRestart Act 1")
     }
+}
+
+class MusicService : Service() {
+    private lateinit var player: MediaPlayer
+
+    override fun onBind(intent: Intent?): IBinder? = null
+
+    override fun onCreate() {
+        player = MediaPlayer.create(this, R.raw.music)
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        player.start()
+        return START_STICKY
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        player.stop()
+    }
+
 }
